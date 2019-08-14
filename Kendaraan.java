d3fea2a45c06cae097bf7c76685cca480317bf4b
rezihardi <rezydoerasta@gmail.com>
2019-08-14 16:27:33 +0700

    this is first commit

---
+++ Kendaraan.java
@@ -0,0 +1,12 @@
+class Kendaraan {
+    String nama;
+    String jenis;
+    int jumlahRoda;
+
+    Kendaraan(String nem, String jen, int rod) {
+        nama = nem;
+        jenis = jen;
+        jumlahRoda = rod;
+    }
+
+}
---
+++ Mobil.java
@@ -0,0 +1,9 @@
+class Mobil extends Kendaraan {
+    int jmlSilinder;
+
+    Mobil(String nem, String jen, int rod, int jmlsil) {
+        super(nem, jen, rod);
+        jmlSilinder = jmlsil;
+    }
+
+}
