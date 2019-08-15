8586077bd400a9c00a4daaa271e1947218b9b0b9
rezihardi <rezydoerasta@gmail.com>
2019-08-15 08:56:25 +0700

    tambah b

--- Main.java
+++ Main.java
@@ -1,14 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        Kendaraan[] b = new Kendaraan[3];
+        // Kendaraan[] b = new Kendaraan[3];
         Mobil[] a = new Mobil[3];
-        
+
         a[0] = new Mobil("xpander", "SUV", 3, 4);
         a[1] = new Mobil("Innova", "MPV", 5, 4);
         a[2] = new Mobil("lancer", "Sport", 4, 6);
 
         for (int i = 0; i < a.length; i++) {
-            System.out.println("spesifikasi dari mobil: " + a[i].nama +", berjenis mobil "+ a[i].jenis +" dengan jumlah roda "+ a[i].jumlahRoda  +", dengan jumlah silinder "+ a[i].jmlSilinder);
+            System.out.println("spesifikasi dari mobil: " + a[i].nama + ", berjenis mobil " + a[i].jenis
+                    + " dengan jumlah roda " + a[i].jumlahRoda + ", dengan jumlah silinder " + a[i].jmlSilinder);
             System.out.println("commit test");
         }
     }
